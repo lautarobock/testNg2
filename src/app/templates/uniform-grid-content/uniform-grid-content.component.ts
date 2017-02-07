927954e57bd4f67c47e279706b275474ac4baf19
Lautaro Cozzani <lautaromail@gmail.com>
2017-02-07 17:15:16 -0300

    fix expander, group, uniform, attachmetn, checkbox, date-picker(partial)

--- src/app/templates/uniform-grid-content/uniform-grid-content.component.ts
+++ src/app/templates/uniform-grid-content/uniform-grid-content.component.ts
@@ -10,26 +10,10 @@ import { RegisterTemplate } from '../template-loader.directive';
 @RegisterTemplate(ContentType.UniformGrid)
 export class UniformGridContentComponent extends AbstractContentComponent implements OnInit {
 
-  // widths = [];
-
   constructor() {
     super();
    }
 
-  ngOnInit() {
-    // let sum = 0;
-    // let total = _.sumBy(uniformGrid.children, function (component) {
-    //     return component.property('Colspan',1);
-    // });
-    // angular.forEach(uniformGrid.children, function (component, idx) {
-    //     if (idx === uniformGrid.children.length - 1) {
-    //         uniformGrid.widths.push(100-sum);
-    //     } else {
-    //         var value = Math.round(100 / total) * component.property('Colspan', 1);
-    //         uniformGrid.widths.push(value);
-    //         sum += value;
-    //     }
-    // });
-  }
+  ngOnInit() { }
 
 }
