ff7a82d8910714a55f4658ef4db9528cc8821913
Lautaro Cozzani <lautaromail@gmail.com>
2017-02-07 16:18:33 -0300

    Migrato to bootstrap document-module

--- src/app/documents/documents.module.ts
+++ src/app/documents/documents.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule }    from '@angular/forms';
-import { MaterialModule } from '@angular/material';
 import { UtilModule } from '../util/util.module';
 import { DocumentsRoutingModule } from './documents-routing.module';
 import { DocumentDetailComponent } from './document-detail/document-detail.component';
@@ -12,7 +11,6 @@ import { TemplatesModule } from '../templates/templates.module';
   imports: [
     CommonModule,
     FormsModule,
-    MaterialModule,
     UtilModule,
     DocumentsRoutingModule,
     TemplatesModule
