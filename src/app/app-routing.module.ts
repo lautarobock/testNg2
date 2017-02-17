4f7bf86db8a98af1bb0816b1143ddad4a2565d2f
Lautaro Cozzani <lautaromail@gmail.com>
2017-02-17 08:26:59 -0300

    lint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 @Component({
-  selector: 'default-view',
+  selector: 'app-default-view',
   template: '<div style="margin: 1em">Welcome to Dataflow</div>'
 })
 export class DefaultComponent {
@@ -11,14 +11,14 @@ export class DefaultComponent {
 const appRoutes: Routes = [{
     path: 'default',
     component: DefaultComponent
-},{
+}, {
     path: '',
     redirectTo: '/default',
     pathMatch: 'full'
 }];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes,{})],
+    imports: [RouterModule.forRoot(appRoutes, {})],
     declarations: [DefaultComponent],
     exports: [RouterModule]
 })
